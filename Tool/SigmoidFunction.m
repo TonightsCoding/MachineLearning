8598d1f2b88639b301b8f927ef42d10ebc1cee12
Exius159 <coin-operated-boi@gmx.net>
2020-02-07 12:13:33 +0100

    added new comment

--- Tool/SigmoidFunction.m
+++ Tool/SigmoidFunction.m
@@ -1,6 +1,6 @@
 function res = SigmoidFunction(x, bias)
    % res - gibt das Ergebnis zurueck
-   % x - 
+   % x - Variable der Funktion
    % bias - Verschiebung in x-Richtung -> Neg (rechts), Pos (links)
 
    res = 1./(1+exp(-(x+bias)));
