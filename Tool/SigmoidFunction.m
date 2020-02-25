fe727d248d6e356a714f5eced2db5b099c32395b
Toni <46779511+TonightsCoding@users.noreply.github.com>
2020-02-25 15:53:27 +0100

    added description of function

--- Tool/SigmoidFunction.m
+++ Tool/SigmoidFunction.m
@@ -1,7 +1,17 @@
-function output = SigmoidFunction(x, bias)
-   % res - gibt das Ergebnis zurueck
-   % x - Variable der Funktion
-   % bias - Verschiebung in x-Richtung -> Neg (rechts), Pos (links)
-
-   output = 1./(1+exp(-(x+bias)));
+function [sigmoid, y] = SigmoidFunction(nice, x1, x2, y1, y2, x, bias)
+   % y - (optional) gibt einzelnen Wert im Punkt x zurueck
+   % sigmoid - Vektor der Dimension (nice) mit den abgetasteten Funktionswerte
+   
+   % nice - Feinheit der Abtastung der Funktion, bestimmt Laenge von (sigmoid)
+   % x1 - untere Grenze der Definitionsbreite (x-Werte) fuer Funktionswerte (default: -12)
+   % x2 - obere Grenze der Definitionsbreite (x-Werte) fuer Funktionswerte (default: 12)
+   % y1 - untere Grenze der Funktionswerte (y-Werte default: -1)
+   % y2 - obere Grenze der Funktionswerte (y-Werte default: 1)
+   % x - (optional) Variable fuer einzelnen Rueckgabewert der Funktion
+   % bias - (optional) Verschiebung in x-Richtung (default: 0) -> Neg (rechts), Pos (links)
+   
+   
+   
+   
+   y = 1./(1+exp(-(x+bias)));
 end
